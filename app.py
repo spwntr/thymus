fd7e4ea53a50b6daa3de42bccf8ed3b1baee280a
spw <spwntr@gmail.com>
2020-02-14 15:05:48 -0700

    [spw] add random question routing and next random question button

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from random import randint
 from flask import Flask, render_template
 import data
 
@@ -22,6 +23,11 @@ def questions():
 @app.route('/question/<string:q_id>/')
 def question(q_id):
     return render_template('question.html', question=data.get_question_by_id(q_id))
+
+
+@app.route('/random')
+def random():
+    return render_template('question.html', question=data.get_question_by_id(randint(1, 427).__str__()))
 
 
 if __name__ == '__main__':
